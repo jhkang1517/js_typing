80a9a52d98200f6dea2086f883c3fd680e27fe35
jhkang1517@gmail.com <jhkang1517@gmail.com>
2021-02-03 16:28:14 +0900

    initial commit

---
+++ lightEffect.js
@@ -0,0 +1,15 @@
+// const key = document.querySelector(".answer");
+
+// function keyboardEventChecker() {
+//     key.addEventListener("keydown", function(k){
+//         const keyCode = k.keyCode;
+//         // console.log(`pushed ${k.key}, and this keyCode is ${keyCode}`)
+//     })
+// }
+
+
+// function init() {
+//     keyboardEventChecker();
+// }
+
+// init();
---
+++ typing.js
@@ -0,0 +1,7 @@
+const aswr = document.querySelector(".answer");
+
+function init() {
+    console.log(quest);
+}
+
+init();
