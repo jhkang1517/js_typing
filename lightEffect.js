c8546ceb75b09103fa6fec756065c98739a94cc0
jhkang1517@gmail.com <jhkang1517@gmail.com>
2021-02-03 17:49:11 +0900

    second commit

--- lightEffect.js
+++ lightEffect.js
@@ -1,15 +1,42 @@
-// const key = document.querySelector(".answer");
+const key = document.querySelector(".answer");
+const kbdLayout = document.querySelector(".lightKeyboard")
 
-// function keyboardEventChecker() {
-//     key.addEventListener("keydown", function(k){
-//         const keyCode = k.keyCode;
-//         // console.log(`pushed ${k.key}, and this keyCode is ${keyCode}`)
-//     })
-// }
+const PRESSED = "pressed";
+const PULLED = "pulled";
+
+function keyboardEventChecker() {
+    key.addEventListener("keydown", function(k){
+        const keyName = k.key;
+        const keyCode = k.keyCode;
+
+        if (keyName === "a") {
+
+            console.log(kbdLayout.childNodes)
+
+            const light = kbdLayout.childNodes[1]
+            light.classList.add(PRESSED)
+
+        }
+        // console.log(`pushed ${k.key}, and this keyCode is ${keyCode}`)
+    })
+
+    key.addEventListener("keyup", function(k){
+        const keyName = k.key;
+        const keyCode = k.keyCode;
+
+        if (keyName === "a") {
+
+            const light = kbdLayout.childNodes[1]
+            light.classList.remove(PRESSED)
+
+        }
+        // console.log(`pushed ${k.key}, and this keyCode is ${keyCode}`)
+    })
+}
 
 
-// function init() {
-//     keyboardEventChecker();
-// }
+function init() {
+    keyboardEventChecker();
+}
 
-// init();
+init();
