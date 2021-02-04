124f0f43d384c6ac61d68618897a6e1146046406
jhkang1517@gmail.com <jhkang1517@gmail.com>
2021-02-04 17:24:04 +0900

    error inspection function develop ver0.1

--- genQuestion.js
+++ genQuestion.js
@@ -1,5 +1,9 @@
 const quest = document.querySelector(".question"); // 영향을 미칠 html 코드를 가져오고
-const questList = ["test1", "test2", "test3"] // 넣을 값들
+const questList = ["Hello World", "test2", "test3"] // 넣을 값들
+
+const COMPLETE = "complete";
+const UNCOMPLETE = "uncomplete";
+const WRONG = "wrong";
 
 
 function genRandom() {
@@ -9,10 +13,20 @@ function genRandom() {
 }
 
 function randomQuestList() {
-    const charSpan = document.createElement('span'); // 값을 담을 태그 생성
+    // const charSpan = document.createElement('span'); // 값을 담을 태그 생성
     const randomNumber = genRandom();
-    charSpan.innerText = questList[randomNumber] // 준비된 태그에 값 삽입 
-    quest.appendChild(charSpan) // 태그안에 태그 삽입 (화면출력)
+    const question = questList[randomNumber].split("");
+
+    var step;
+    for (step = 0; step < question.length; step++) {
+        const tempSpan = document.createElement('span');
+        tempSpan.classList.add(UNCOMPLETE);
+        tempSpan.innerText = question[step];
+        quest.appendChild(tempSpan);
+    } 
+    
+    // charSpan.innerText = questList[randomNumber] // 준비된 태그에 값 삽입 
+    // quest.appendChild(charSpan) // 태그안에 태그 삽입 (화면출력)
 }
 
 function init() {
--- typing.js
+++ typing.js
@@ -1,7 +1,35 @@
 const aswr = document.querySelector(".answer");
 
-function init() {
+function updateAnswer() {
+    const checkList = aswr.value;
+    const lencheckList = checkList.length;
+
     console.log(quest);
+
+    var step;
+    for (step = 0; step < lencheckList; step++) {
+
+        if (quest.textContent[step] === checkList[step]) {
+            quest.children[step].className = COMPLETE;
+        } else {
+            quest.children[step].className = WRONG;
+        }
+    }
+
+
+    // console.log(aswr.value);
+}
+
+function focusInput(){
+    console.log(aswr.textContent);
+    console.log(quest.textContent);
+
+    aswr.addEventListener("keyup", updateAnswer)
+
+}
+
+function init() {
+    focusInput();
 }
 
 init();
