6b1de6f3df5637d9a9968af67d174392e1e8e19f
jhkang1517@gmail.com <jhkang1517@gmail.com>
2021-02-04 15:04:37 +0900

    light effect function complete

--- lightEffect.js
+++ lightEffect.js
@@ -6,27 +6,23 @@ const PRESSED = "pressed";
 function keyboardEventChecker() {
     key.addEventListener("keydown", function(k){
         const keyName = k.key;
-        const keyCode = k.keyCode;
-
-        console.log(k.key)
-
         const light = document.getElementsByClassName("light" + keyName.toUpperCase());
             
             var step;
             for (step = 0; step < light.length; step++) {
-                light[step].classList.add(PRESSED);
+                // light[step].classList.add(PRESSED); // class 추가는 css 우선순위 문제로 작동을 안함.. 왜..?
+                light[step].style = "color: rgb(255, 187, 0)" // style 추가로 대체
             }
     })
 
     key.addEventListener("keyup", function(k){
         const keyName = k.key;
-        const keyCode = k.keyCode;
-
         const light = document.getElementsByClassName("light" + keyName.toUpperCase());
             
             var step;
             for (step = 0; step < light.length; step++) {
-                light[step].classList.remove(PRESSED);
+                // light[step].classList.remove(PRESSED); // class 추가는 css 우선순위 문제로 작동을 안함.. 왜..?
+                light[step].style = ""
             }
     })
 }
