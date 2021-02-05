6454eb656e83e4dabaf704e9b2b91b46d8a7a0a3
jhkang1517@gmail.com <jhkang1517@gmail.com>
2021-02-05 10:22:58 +0900

    typo error check function ver0.2

--- typing.js
+++ typing.js
@@ -1,29 +1,33 @@
 const aswr = document.querySelector(".answer");
 
+function resetColor(){
+    var step;
+    for (step = 0; step < quest.childNodes.length; step++) {
+        quest.children[step].className = UNCOMPLETE;
+    }
+}
+
 function updateAnswer() {
     const checkList = aswr.value;
     const lencheckList = checkList.length;
+    const nowPosition = checkList[lencheckList-1];
 
-    console.log(quest);
+    resetColor();
 
     var step;
     for (step = 0; step < lencheckList; step++) {
 
         if (quest.textContent[step] === checkList[step]) {
             quest.children[step].className = COMPLETE;
-        } else {
+        } 
+         else {
             quest.children[step].className = WRONG;
         }
     }
 
-
-    // console.log(aswr.value);
 }
 
 function focusInput(){
-    console.log(aswr.textContent);
-    console.log(quest.textContent);
-
     aswr.addEventListener("keyup", updateAnswer)
 
 }
